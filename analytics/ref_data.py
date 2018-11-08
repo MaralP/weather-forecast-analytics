50eea5398a2d4ae1b63e9c12af60c2a9012b9e16
unknown <maral.panahi@gmail.com>
2018-11-08 14:05:09 -0800

    Add reference city data.

---
+++ analytics/ref_data.py
@@ -0,0 +1,9 @@
+
+cities = {6173331: 'Vancouver',
+          6085772 : 'Nanaimo',
+          6111632 : 'Port Alberni',
+          7281936 : 'West Kelowna',
+          5946768 : 'Edmonton',
+          6077243 : 'Montreal',
+          6094817 : 'Ottawa',
+          5913490 : 'Calgary'}
